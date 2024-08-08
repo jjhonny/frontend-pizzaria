8e2522334b1c77ff6ef3cee818b53a21481bea71
jjhonny <ojhonny1@hotmail.com>
2024-08-08 09:47:41 -0400

    feat: create login page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/home.module.scss";
+
+import logoImg from "../../public/logo.svg";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <>
-      <div>
-        <h1>Sujeito Pizza</h1>
+      <Head>
+        <title>Pizzaria - Faça seu login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Sujeito Pizarria" />
+
+        <div className={styles.login}>
+          <form>
+            <Input type="text" placeholder="Digite seu email" />
+            <Input type="password" placeholder="Digite sua senha" />
+            <Button type="submit" loading={false}>
+              Acessar
+            </Button>
+          </form>
+          <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+        </div>
       </div>
     </>
   );
