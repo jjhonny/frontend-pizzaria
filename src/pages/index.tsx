67b3eab9e48dfaf7900b5445d1d4c23118b8429f
jjhonny <ojhonny1@hotmail.com>
2024-08-08 14:22:40 -0400

    feat: creates AuthContext

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,8 +6,23 @@ import logoImg from "../../public/logo.svg";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { FormEvent, useContext } from "react";
+import { AuthContext } from "@/contexts/AuthContext";
 
 export default function Home() {
+  const { signIn } = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    let data = {
+      email: "algum@teste.com",
+      password: "12341234",
+    };
+
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -18,7 +33,7 @@ export default function Home() {
 
         <div className={styles.login}>
           <h1>Login</h1>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input type="text" placeholder="Digite seu email" />
             <Input type="password" placeholder="Digite sua senha" />
             <Button type="submit" loading={false}>
