65d69fd265cd8ba309ca7a484cddd9d4390c59e6
jjhonny <ojhonny1@hotmail.com>
2024-08-08 10:01:47 -0400

    feat: creates signup page

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -2,31 +2,32 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "@/styles/home.module.scss";
 
-import logoImg from "../../public/logo.svg";
+import logoImg from "../../../public/logo.svg";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Pizzaria - Faça seu login</title>
+        <title>Pizzaria - Faça seu cadastro agora!</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizarria" />
 
         <div className={styles.login}>
-          <h1>Login</h1>
+          <h1>Cadastrar Conta</h1>
           <form>
+            <Input type="text" placeholder="Digite seu nome" />
             <Input type="text" placeholder="Digite seu email" />
             <Input type="password" placeholder="Digite sua senha" />
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
-          <Link className={styles.text} href="/signup">
-            <span>Não possui uma conta? Cadastre-se</span>
+          <Link className={styles.text} href="/">
+            <span>Já possui uma conta? Faça login!</span>
           </Link>
         </div>
       </div>
